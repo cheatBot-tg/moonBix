1dab9b668dde88bdaa15636c3cc9c59600c47ffb
cheatBot-tg <asfakulsiam05@gmail.com>
2024-10-12 18:12:37 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,37 @@
 import os
-import shutil
 import subprocess
 
-# Check if dependencies are installed
-if not os.path.exists('venv/Lib/site-packages/installed'):
-    if os.path.exists('requirements.txt'):
-        print("Installing wheel for faster installing...")
-        subprocess.run(['pip', 'install', 'wheel'])  # Install wheel
+# Проверка на наличие папки venv
+if not os.path.isdir("venv"):
+    print("Creating virtual environment...")
+    subprocess.run(["python3", "-m", "venv", "venv"])
 
+print("Activating virtual environment...")
+subprocess.run(["source", "venv/bin/activate"], shell=True)
+
+# Проверка на наличие установленного флага в виртуальном окружении
+if not os.path.isfile("venv/lib/python3.12/site-packages/installed"):  # Update the Python version if different
+    if os.path.isfile("requirements.txt"):
+        print("Installing wheel for faster installation...")
+        subprocess.run(["pip", "install", "wheel"])
         print("Installing dependencies...")
-        subprocess.run(['pip', 'install', '-r', 'requirements.txt'])  # Install dependencies
-
-        # Create an 'installed' marker file
-        with open('venv/Lib/site-packages/installed', 'w') as f:
-            pass
+        subprocess.run(["pip", "install", "-r", "requirements.txt"])
+        # Mark dependencies as installed
+        with open("venv/lib/python3.12/site-packages/installed", "w") as f:  # Update the Python version if different
+            f.write('')
     else:
         print("requirements.txt not found, skipping dependency installation.")
 else:
     print("Dependencies already installed, skipping installation.")
 
-# Check if .env file exists and copy from .env-example if not
-if not os.path.exists('.env'):
+if not os.path.isfile(".env"):
     print("Copying configuration file...")
-    shutil.copy('.env-example', '.env')
+    subprocess.run(["cp", ".env-example", ".env"])
 else:
     print("Skipping .env copying.")
 
-# Start the bot
 print("Starting the bot...")
-subprocess.run(['python', 'main.py'])  # Execute main.py
+subprocess.run(["python3", "main.py"])
 
 print("done")
 print("PLEASE EDIT .ENV FILE")
